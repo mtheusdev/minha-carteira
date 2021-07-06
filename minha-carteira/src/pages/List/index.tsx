aaf3a4a9f434f5207ef585a9957609f5e3236a02
mtheusdev <matheus.santos@edu.udesc.br>
2021-07-05 21:29:46 -0300

    criando filtros

--- minha-carteira/src/pages/List/index.tsx
+++ minha-carteira/src/pages/List/index.tsx
@@ -1,20 +1,43 @@
 import React from 'react'
-import { Container, Content } from './style'
+import { Container, Content, Filters } from './style'
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from './../../components/HistoryFinanceCard/index';
 
 const List: React.FC = () => {
-  const options = [
-    {
-      value: 'Seila', label: 'seilla'
-    }
-  ]
+
+  const months = [
+    {value: 7, label: 'Julho'},
+    {value: 8, label: 'Agosto'},
+    {value: 9, label: 'Setembro'}
+  ];
+
+  const years = [
+    {value: 2020, label: 2020},
+    {value: 2021, label: 2021},
+    {value: 2022, label: 2022}
+  ];
+
   return (
     <Container>
       <ContentHeader title="Entradas" lineColor="#484">
-          <SelectInput options={options}/>
+          <SelectInput options={months}/>
+          <SelectInput options={years}/>
       </ContentHeader>
+      <Filters>
+        <button 
+          type="button"
+          className="tag-filter tag-filter-reccurent"
+        >
+          Recorrentes
+        </button>
+        <button 
+          type="button"
+          className="tag-filter tag-filter-eventual"
+        >
+          Eventuais
+        </button>
+      </Filters>
       <Content>
         <HistoryFinanceCard
           tagColor="#E44C4E"
